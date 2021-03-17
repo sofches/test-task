6db3a4adcb6a26d9c31bff6834769043661acc73
sofches <sofches@gmail.com>
2021-03-17 23:47:37 +0200

    added slider

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
+// video button code
 const video = document.getElementById("myVideo");
 const btn = document.getElementById("myBtn");
 
@@ -10,3 +11,30 @@ function playPause() {
       btn.innerHTML = `<i class="bi bi-play-circle"></i>`;
     }
   }
+
+// slider code
+new Swiper('.swiper-container',{
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  grabCursor: true,
+  spaceBetween: 15,
+  initialSlide: 1,
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+    disabledOnInteraction: true
+  },
+
+  speed: 800,
+
+
+});
